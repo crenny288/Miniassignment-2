898ee2e583fd138a965eb3ca19b4658083e08ad4
Ben Sterrett <raynjamin@gmail.com>
2016-04-24 17:24:38 -0400

    Adding basic filse

---
+++ src/com/theironyard/clt/Main.java
@@ -0,0 +1,12 @@
+package com.theironyard.clt;
+
+public class Main {
+
+    /**
+     *
+     * @param args: The command line arguments to the Main function.
+     */
+    public static void main(String[] args) {
+
+    }
+}
